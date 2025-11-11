7b10585e24818dedbbff13a4f143b5c1fed65260
Eulalie <eulalieflambard@gmail.com>
2025-11-11 13:41:51 +0100

    Update script.js

--- alice&thomas/script.js
+++ alice&thomas/script.js
@@ -1,13 +1,3 @@
-// Petit effet d’animation au clic sur les boutons du menu
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", (e) => {
-    const target = document.querySelector(e.target.getAttribute("href"));
-    if (target) {
-      e.preventDefault();
-      window.scrollTo({
-        top: target.offsetTop - 50,
-        behavior: "smooth"
-      });
-    }
-  });
+document.getElementById("btn").addEventListener("click", () => {
+  alert("Ton site fonctionne 👍");
 });
